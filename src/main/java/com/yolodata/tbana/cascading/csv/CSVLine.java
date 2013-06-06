e8518409f22b7ca38b30520d24b939d818d91783
Matteus <matteus.klich@gmail.com>
2013-06-06 20:23:52 +0200

    CSVLine is a scheme that uses CSVInputFormat to read multiline csv files

--- src/main/java/com/yolodata/tbana/cascading/csv/CSVLine.java
+++ src/main/java/com/yolodata/tbana/cascading/csv/CSVLine.java
@@ -1,5 +1,45 @@
 package com.yolodata.tbana.cascading.csv;
 
-public class CSVLine {
 
+import cascading.flow.FlowProcess;
+import cascading.scheme.Scheme;
+import cascading.scheme.SinkCall;
+import cascading.scheme.SourceCall;
+import cascading.tap.Tap;
+import cascading.tuple.Fields;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.RecordReader;
+
+import java.io.IOException;
+
+public class CSVLine extends Scheme<JobConf, RecordReader, OutputCollector, Object[], Object[]> {
+
+    /** Field DEFAULT_SOURCE_FIELDS */
+    public static final Fields DEFAULT_SOURCE_FIELDS = new Fields( "offset", "columns" );
+
+    public CSVLine()
+    {
+        super( DEFAULT_SOURCE_FIELDS );
+    }
+
+    @Override
+    public void sourceConfInit(FlowProcess<JobConf> flowProcess, Tap<JobConf, RecordReader, OutputCollector> tap, JobConf conf) {
+
+    }
+
+    @Override
+    public void sinkConfInit(FlowProcess<JobConf> flowProcess, Tap<JobConf, RecordReader, OutputCollector> tap, JobConf conf) {
+
+    }
+
+    @Override
+    public boolean source(FlowProcess<JobConf> flowProcess, SourceCall<Object[], RecordReader> sourceCall) throws IOException {
+        return false;
+    }
+
+    @Override
+    public void sink(FlowProcess<JobConf> flowProcess, SinkCall<Object[], OutputCollector> sinkCall) throws IOException {
+
+    }
 }

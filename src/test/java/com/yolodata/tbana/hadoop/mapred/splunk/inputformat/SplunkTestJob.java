415d4a0748f14073e1d9137fa85b7fa129d289ad
Matteus <matteus.klich@gmail.com>
2013-06-21 09:47:19 +0200

    Added import of missing packages

--- src/test/java/com/yolodata/tbana/hadoop/mapred/splunk/inputformat/SplunkTestJob.java
+++ src/test/java/com/yolodata/tbana/hadoop/mapred/splunk/inputformat/SplunkTestJob.java
@@ -1,5 +1,7 @@
 package com.yolodata.tbana.hadoop.mapred.splunk.inputformat;
 
+import com.yolodata.tbana.hadoop.mapred.splunk.SplunkConf;
+import com.yolodata.tbana.hadoop.mapred.splunk.SplunkInputFormat;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;

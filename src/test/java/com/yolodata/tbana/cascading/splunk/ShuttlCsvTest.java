c37f9dd17249b947ce816955e6ccc6156e6b0aea
Matteus <matteus.klich@gmail.com>
2013-07-06 08:27:56 -0700

    Improved test

--- src/test/java/com/yolodata/tbana/cascading/splunk/ShuttlCsvTest.java
+++ src/test/java/com/yolodata/tbana/cascading/splunk/ShuttlCsvTest.java
@@ -7,9 +7,13 @@ import cascading.pipe.Pipe;
 import cascading.scheme.hadoop.TextLine;
 import cascading.tap.SinkMode;
 import cascading.tap.hadoop.Hfs;
-import cascading.tuple.TupleEntryIterator;
-import com.yolodata.tbana.hadoop.mapred.util.ArrayListTextWritable;
-import org.apache.hadoop.io.Text;
+import com.yolodata.tbana.testutils.FileSystemTestUtils;
+import com.yolodata.tbana.testutils.FileTestUtils;
+import com.yolodata.tbana.testutils.HadoopFileTestUtils;
+import com.yolodata.tbana.testutils.TestUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -17,49 +21,73 @@ import java.util.Properties;
 
 public class ShuttlCsvTest extends CascadingTestCase {
 
-    String testData = "src/test/resources/multilineCSV.csv";
+    private FileSystem fs;
 
-    String outputPath = "build/testTMP";
-
-    @Test
-    public void testShuttlCsv() throws IOException {
-    runCSVLine("csvtest", testData);
+    @Override
+    public void setUp() throws Exception {
+        fs = FileSystem.getLocal(new Configuration());
+        fs.delete(new Path(TestUtils.TEST_FILE_PATH), true);
     }
 
-    public void runCSVLine( String path, String inputData) throws IOException
+    @Test
+    public void testShuttlDirectory() throws IOException, InstantiationException, IllegalAccessException {
+        Path directory = FileSystemTestUtils.createEmptyDir(fs);
+
+        String[] header = {"header", "_raw"};
+
+        String file1Content = "header,_raw\n" +
+                "\"ImCESTvlhu\",\"LOjZxHYGZy\"\n" +
+                "\"kqYkcFhbSB\",\"RRLjuHCHze\"\n" +
+                "\"kmeEaOcKTx\",\"mvIPrMSOSS\"\n" +
+                "\"lzzPLYFGFU\",\"sGHTPVsYlF\"\n" +
+                "\"zpUxVlTaAq\",\"ysoUYuyZKO\"";
+        String file2Content = "header,_raw\n" +
+                "\"fkBkKfCkuT\",\"BRlSkqHmHe\"\n" +
+                "\"dWDJViEuot\",\"LcdkTQBLmu\"\n" +
+                "\"ovQoDFATdn\",\"YewByxPXqN\"\n" +
+                "\"tKBxjsSZmV\",\"luuOivALWj\"\n" +
+                "\"mssAbiUnub\",\"NeYnIlDMdW\"";
+
+        Path file1 = HadoopFileTestUtils.createPath(directory.toString(),"file1.csv");
+        Path file2 = HadoopFileTestUtils.createPath(directory.toString(),"file2.csv");
+        HadoopFileTestUtils.createFileWithContent(fs,file1,file1Content);
+        HadoopFileTestUtils.createFileWithContent(fs,file2,file2Content);
+
+        Path outputPath = new Path(FileTestUtils.getRandomTestFilepath());
+        runCascadingJob(directory,outputPath);
+
+        String expectedContent = "0\t[header, _raw]\n" +
+                "12\t[ImCESTvlhu, LOjZxHYGZy]\n" +
+                "38\t[kqYkcFhbSB, RRLjuHCHze]\n" +
+                "64\t[kmeEaOcKTx, mvIPrMSOSS]\n" +
+                "90\t[lzzPLYFGFU, sGHTPVsYlF]\n" +
+                "116\t[zpUxVlTaAq, ysoUYuyZKO]\n" +
+                "153\t[fkBkKfCkuT, BRlSkqHmHe]\n" +
+                "179\t[dWDJViEuot, LcdkTQBLmu]\n" +
+                "205\t[ovQoDFATdn, YewByxPXqN]\n" +
+                "231\t[tKBxjsSZmV, luuOivALWj]\n" +
+                "257\t[mssAbiUnub, NeYnIlDMdW]\n";
+
+        String actualResults = HadoopFileTestUtils.readMapReduceOutputFile(fs,outputPath);
+
+        assertEquals(expectedContent,actualResults);
+    }
+
+    public Flow runCascadingJob( Path inputPath, Path outputPath) throws IOException
     {
         Properties properties = new Properties();
 
         ShuttlCsv inputScheme = new ShuttlCsv();
         TextLine outputScheme = new TextLine();
 
-        Hfs input = new Hfs( inputScheme, inputData );
-        Hfs output = new Hfs( outputScheme, outputPath + "/quoted/" + path, SinkMode.REPLACE );
+        Hfs input = new Hfs(inputScheme,inputPath.toString());
+        Hfs output = new Hfs(outputScheme,outputPath.toString(),SinkMode.REPLACE);
 
         Pipe pipe = new Pipe( "test" );
         Flow flow = new HadoopFlowConnector( properties ).connect( input, output, pipe );
 
         flow.complete();
 
-        validateLength( flow, 4, 2 ); // The file contains 4 rows, however there are only 3 CSV rows (inc the header row)
-
-
-        TupleEntryIterator iterator = flow.openSource();
-
-        ArrayListTextWritable expected = new ArrayListTextWritable();
-
-        expected.add(new Text("header1"));
-        expected.add(new Text("header2"));
-        assertEquals(expected, iterator.next().getTuple().getObject(1));
-
-        expected.clear();
-        expected.add(new Text("Column1"));
-        expected.add(new Text("Column 2 using\ntwo rows"));
-        assertEquals(expected, iterator.next().getTuple().getObject(1));
-
-        expected.clear();
-        expected.add(new Text("c1"));
-        expected.add(new Text("c2"));
-        assertEquals(expected, iterator.next().getTuple().getObject(1));
+        return flow;
     }
 }

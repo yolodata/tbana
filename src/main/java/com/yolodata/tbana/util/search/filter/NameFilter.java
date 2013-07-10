8eb33ce09b1b2eb4b4b88e600d6a11ad34e0b6e5
Matteus <matteus.klich@gmail.com>
2013-07-10 12:41:52 -0700

    Added wlidcard support for NameFilter

--- src/main/java/com/yolodata/tbana/util/search/filter/NameFilter.java
+++ src/main/java/com/yolodata/tbana/util/search/filter/NameFilter.java
@@ -3,6 +3,8 @@ package com.yolodata.tbana.util.search.filter;
 import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NameFilter implements SearchFilter{
 
@@ -26,11 +28,28 @@ public class NameFilter implements SearchFilter{
 
     private boolean matchesAnyName(String name) {
         for(String n : names) {
+            // add regex if string contains *
+            if(containsWildcard(n))
+               return doRegexMatching(n, name);
+            Pattern.compile(name);
             if(n.equals(name))
                 return true;
         }
 
         return false;
+    }
+
+    private boolean doRegexMatching(String namePattern, String nameToMatch) {
+
+        namePattern = namePattern.replace("*","(.*)");
+        namePattern = ("^".concat(namePattern)).concat("$");
+        Pattern p = Pattern.compile(namePattern);
+        Matcher m = p.matcher(nameToMatch);
+        return m.matches();
+    }
+
+    private boolean containsWildcard(String name) {
+        return name.contains("*");
     }
 
 
--- src/test/java/com/yolodata/tbana/util/search/filter/NameFilterTest.java
+++ src/test/java/com/yolodata/tbana/util/search/filter/NameFilterTest.java
@@ -7,13 +7,38 @@ import static org.junit.Assert.assertEquals;
 public class NameFilterTest {
 
     @Test
-    public void testExtension() throws Exception {
+    public void testFilterWithFileName() throws Exception {
         String csvFilePath = "/path/to/csvFile.csv";
 
         NameFilter csvFilter = new NameFilter("csvFile.csv");
 
+        assertEquals(true,csvFilter.accept(csvFilePath));
+    }
+
+
+    @Test
+    public void testFilterWithWildcard() throws Exception {
+        String csvFilePath = "/path/to/csvFile.csv";
+
+        String [] acceptedNames = {"csvFile.*","csv*.csv"};
+        NameFilter csvFilter = new NameFilter(acceptedNames);
 
         assertEquals(true,csvFilter.accept(csvFilePath));
+    }
+
+    @Test
+    public void testFilterWithMultipleAcceptedNames() throws Exception {
+        String acceptedName = "path/to/directory";
+        String acceptedName2 = "path/to/accepted";
+        String notAccepted = "path/to/something";
+
+
+        String [] acceptedNames = {"directory","acc*"};
+        NameFilter csvFilter = new NameFilter(acceptedNames);
+
+        assertEquals(true,csvFilter.accept(acceptedName));
+        assertEquals(true,csvFilter.accept(acceptedName2));
+        assertEquals(false,csvFilter.accept(notAccepted));
 
     }
 }

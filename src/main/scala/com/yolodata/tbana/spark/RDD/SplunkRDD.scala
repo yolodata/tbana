6fab4c7193eef2145b57a1f984f6a7f3e3e30ae3
Matteus <matteus.klich@gmail.com>
2013-07-18 10:40:04 -0700

    Implemented SplunkRDD. It uses the SplunkInputFormat to create a RDD for Spark

--- src/main/scala/com/yolodata/tbana/spark/RDD/SplunkRDD.scala
+++ src/main/scala/com/yolodata/tbana/spark/RDD/SplunkRDD.scala
@@ -1,5 +1,25 @@
 package com.yolodata.tbana.spark.RDD
 
-class SplunkRDD {
+import spark.rdd.HadoopRDD
+import spark.SparkContext
+import org.apache.hadoop.mapred.JobConf
+import com.yolodata.tbana.hadoop.mapred.splunk.{SplunkConf, SplunkInputFormat}
+import com.yolodata.tbana.hadoop.mapred.util.ArrayListTextWritable
+import org.apache.hadoop.io.LongWritable
+import com.yolodata.tbana.cascading.splunk.SplunkDataQuery
 
+object SplunkRDD {
+  def apply(context : SparkContext, query : SplunkDataQuery) : SplunkRDD = {
+    apply(context, query, new JobConf())
+  }
+
+  def apply(context: SparkContext, query : SplunkDataQuery, jobConf : JobConf) : SplunkRDD = {
+    jobConf.set(SplunkConf.SPLUNK_EARLIEST_TIME,query.getEarliestTimeString)
+    jobConf.set(SplunkConf.SPLUNK_LATEST_TIME,query.getEarliestTimeString)
+    jobConf.set(SplunkConf.SPLUNK_SEARCH_QUERY,query.getSplunkQuery)
+
+    new SplunkRDD(context,jobConf)
+  }
 }
+
+case class SplunkRDD(@transient private val ctxt : SparkContext, @transient conf : JobConf, @transient splits : Int = 2 ) extends HadoopRDD(ctxt, conf, classOf[SplunkInputFormat], classOf[LongWritable], classOf[ArrayListTextWritable], splits)
--- src/test/scala/com/yolodata/tbana/spark/RDD/SplunkRDDTest.scala
+++ src/test/scala/com/yolodata/tbana/spark/RDD/SplunkRDDTest.scala
@@ -3,10 +3,32 @@ package com.yolodata.tbana.spark.RDD
 import org.scalatest.FunSuite
 import spark.SparkContext
 import com.yolodata.tbana.spark.LocalSparkContext
+import com.yolodata.tbana.testutils._
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.fs.{FileSystem, Path}
 
 class SplunkRDDTest extends FunSuite with LocalSparkContext {
   test("basic functionality") {
     sc = new SparkContext("local", "test")
-    val rdd = new SplunkRDD()
+    val conf : JobConf = new JobConf(TestConfigurations.getConfigurationWithSplunkConfigured)
+    val rdd = new SplunkRDD(sc, conf)
+
+    assert(rdd.count() === 6)
+
+    val path: Path = new Path(TestUtils.TEST_FILE_PATH.concat("SplunkRDD.out"))
+    rdd.saveAsTextFile(path.toString)
+
+    val fs : FileSystem = FileSystem.getLocal(conf)
+    val actual = HadoopFileTestUtils.readMapReduceOutputFile(fs,path)
+
+    val expected = "(0,[_raw])\n" +
+      "(1,[count=4])\n" +
+      "(2,[count=3])\n" +
+      "(3,[count=2])\n" +
+      "(4,[count=1])\n" +
+      "(5,[count=0])\n"
+
+    assert(expected === actual, "The read content from splunk is not correct")
+
   }
 }

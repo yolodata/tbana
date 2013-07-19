99f61426ffc7cb8465bf87b4322f4e09dd607f79
Konrad Rzezniczak <konradr@kth.se>
2013-07-19 14:51:19 -0700

    Added clojure support and a storm spout and bolt for splunk, clojureTest task not working atm.

---
+++ src/integration-test/clojure/com/yolodata/tbana/storm/splunkIntegrationTest.clj
@@ -0,0 +1,29 @@
+(ns com.yolodata.tbana.storm.splunk-integration-test
+  (:use [clojure test])
+  (:use [backtype.storm testing])
+  (:use [com.yolodata.tbana.storm.splunk]))
+
+(def serviceArgs {"username" "admin",
+                  "password" "changeIt",
+                  "host" "localhost",
+                  "port" (int 9050)})
+
+(def exportArgs {"earliest_time" "0",
+                 "latest_time" "now",
+                 "search_mode" "normal"})
+
+(defn mk-topology []
+  (topology
+    {"1" (spout-spec (splunk-csv-spout serviceArgs "search sourcetype=mock" exportArgs)
+           :p 1)}
+    {"2" (bolt-spec {"1" :shuffle}
+           split-event
+           :p 1)}))
+
+;;(deftest splunk-spout-test
+  ;;(with-local-cluster [cluster]
+    ;;(.submitTopology cluster "splunk-integration-test" {TOPOLOGY-DEBUG true} (mk-topology))))
+
+(defn -main
+  ([]
+    (run-local!)))
---
+++ src/main/clojure/com/yolodata/tbana/storm/splunk.clj
@@ -0,0 +1,35 @@
+(ns com.yolodata.tbana.storm.splunk
+  (:import [backtype.storm StormSubmitter LocalCluster])
+  (:import (com.splunk Service ServiceArgs JobExportArgs))
+  (:require [opencsv-clj.core :as csv])
+  (:use [backtype.storm clojure config])
+  (:gen-class :main true))
+
+(defn connectToSplunk [serviceArgs]
+  (Service/connect serviceArgs))
+
+(defn createExportStream [service query exportArgs]
+  (.export service query exportArgs))
+
+(defspout splunk-csv-spout ["event"] {:params [serviceArgs splunkQuery exportArgs]}
+  [conf contex collector]
+  (let [service (connectToSplunk serviceArgs)
+        exportArgsWithCSV (conj exportArgs {"output_mode" "csv"}) ;;overrides output format to csv
+        exportStream (createExportStream service splunkQuery exportArgsWithCSV)
+        csvSeq (atom (csv/parse (java.io.InputStreamReader. exportStream) :mapped true))]
+    ;;(do (println "***\n***\n" (first csvSeq) "\n***\n***\n\n" exportArgsWithCSV "\n\n"))
+    (spout
+      (nextTuple []
+        (Thread/sleep 500)
+        (emit-spout! collector [(first (swap! csvSeq rest))]))
+      (ack [id]
+        ;;TODO: FIX THIS
+        ))))
+
+(defbolt split-event ["key" "value"] [tuple collector]
+  (let [values (.getValue tuple 0)]
+    (doseq [pair values]
+      (emit-bolt! collector pair :anchor tuple))
+    (ack! collector tuple)
+    ))
+
